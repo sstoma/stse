2dc22efab55da98cee9f8ded1cd2b3219075fa0d
sstoma <sstoma@users.noreply.github.com>
2010-06-17 17:11:01 +0000

    * analysis test

--- src/stse/examples/10_06_17_analysis_schmoo.py
+++ src/stse/examples/10_06_17_analysis_schmoo.py
@@ -6,6 +6,8 @@ stse_svn_revision = 54
 
 from openalea.stse.gui.compartment_editor import start_gui
 from openalea.stse.structures.algo.walled_tissue import avg_cell_property
+from openalea.stse.structures.algo.walled_tissue import calculate_cell_surface,\
+    calculate_wall_length, cell_edge2wv_edge, cell_centers
 
 import sys
 import copy
@@ -46,7 +48,15 @@ if __name__ == '__main__':
     for i in expression_channels2cell_types.keys():
         print expression_channels2cell_types[ i ], avg_cell_property(wt=mesh, property="custom_cell_property1", property_filter="cell_type", property_filter_value=expression_channels2cell_types[ i ], consider_surface=False)
     
+    #getting ratio between cytoplasm surface and nucleus surface
+    cs = 0.
+    ns = 0.
+    
     for i in mesh.cells():
-        pass
+        if mesh.cell_property(i, "cell_type") == "C":
+            cs += calculate_cell_surface(mesh, i)
+        elif mesh.cell_property(i, "cell_type") == "D":
+            ns += calculate_cell_surface(mesh, i)
+    print "Ratio between cell/nucleus surface: ", cs/ns
     
     
