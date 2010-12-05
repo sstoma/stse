f39c637ae3ee071eb8045c2db13fe56a39f426e8
sstoma <sstoma@users.noreply.github.com>
2010-12-05 10:01:24 +0000

    * correction of .sh to work on os x

--- data/10-12-01-3d,initial,format,x3d/validate_dtd.sh
+++ data/10-12-01-3d,initial,format,x3d/validate_dtd.sh
@@ -6,7 +6,7 @@ dovalidate ()
   echo '--- Validating ' "$1"
   # Use always local DTD, fixed (for ClipPlane etc.) and in the future
   # validating also STSE extensions.
-  sed "$1" -e 's|http://www.web3d.org/specifications/x3d-3.2.dtd|file://'`pwd`'/x3d-3.2.dtd|' > validate_dtd-tmp.x3d
+  sed -e 's|http://www.web3d.org/specifications/x3d-3.2.dtd|file://'`pwd`'/x3d-3.2.dtd|' "$1" > validate_dtd-tmp.x3d
   set +e
   xmllint --noout --postvalid validate_dtd-tmp.x3d 2>&1 | grep --invert-match 'Content model of ProtoBody is not determinist'
   set -e
