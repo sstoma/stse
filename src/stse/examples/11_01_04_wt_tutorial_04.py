d1c726b80ff9156dbf8b9453b843efb6b1f46248
netfreex <netfreex@38f2cb1c-3f93-4bcc-9796-8684ec2b1f8e>
2011-02-05 00:57:07 +0000

    Drobna poprawka tutorialu wt_04.

--- src/stse/examples/11_01_04_wt_tutorial_04.py
+++ src/stse/examples/11_01_04_wt_tutorial_04.py
@@ -1,19 +1,14 @@
 #!/usr/bin/env python
 """This is the file demonstrating how to load WT2D structure and  display it using mayavi framework.
-
 :todo:
     Nothing.
-
 :bug:
     None known.
-    
 :organization:
-    Humboldt University
+    Humboldt University"""
 
-"""
 # Module documentation variables:
-__authors__="""Szymon Stoma    
-"""
+__authors__="""Szymon Stoma"""
 __contact__="<Your contact>"
 __license__="Cecill-C"
 __date__=""
@@ -28,8 +23,7 @@ import os
 from matplotlib.pyplot import scatter, legend, show, figure, xlabel, ylabel
 
 from openalea.stse.gui.compartment_viewer import start_gui
-from openalea.stse.structures.algo.walled_tissue import calculate_cell_surface,\
-    cell_center
+from openalea.stse.structures.algo.walled_tissue import calculate_cell_surface, cell_center
 from openalea.stse.structures.algo.walled_tissue import *
 from openalea.stse.structures.algo.walled_tissue_dscs import *
 from openalea.stse.structures.algo.walled_tissue_cfd import *
@@ -103,24 +97,30 @@ def test_if_division_is_possible( wt2d, shape ):
 
 if __name__ == '__main__':
     window = start_gui()
-    # be sure to have STSE_DIR set pointing to the root of STSE 
+    # Be sure to have STSE_DIR set pointing to the root of STSE.
     stse_path = os.getenv("STSE_DIR")
     data_dir = "data/11-01-04-1cellTest"
+    
     tissue_filename = "tissue01"
     
-    # proxy to actions
+    # Proxy to actions.
     a2 = window.actions['file_load_walled_tissue']
     
-    # loading geometry
-    a2.load( join( stse_path, data_dir, tissue_filename ) )
+    # Loading geometry.
+    a2.load(join(stse_path, data_dir, tissue_filename))
     
-    # proxy to tissue
+    # Proxy to tissue.
     mesh = window._voronoi_wt
 
     print "# Example (geometrical properties of the mesh - in pixels):"
-    print "  Example cell surface:", calculate_cell_surface(mesh, mesh.cells()[0] )
-    print "  Example cell center:", cell_center(mesh, mesh.cells()[0] )
+    print "  Example cell surface: ", calculate_cell_surface(mesh, mesh.cells()[0])
+    print "  Example cell center: ", cell_center(mesh, mesh.cells()[0])
 
+<<<<<<< .mine
+    # Visualisation setting.
+    window._cell_scalars_active = False
+    
+    =======
     # visualisation setting
     window._cell_scalars_active = False
     mesh = window._voronoi_wt
@@ -131,3 +131,4 @@ if __name__ == '__main__':
         chd_surface(mesh, 200, dcs_shortest_wall)
         i = i + 1
         window.update_vtk_from_voronoi(  )
+>>>>>>> .r204
